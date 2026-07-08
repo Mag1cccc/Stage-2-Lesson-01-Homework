26b3cf9486e265553383951897064930a427a06b
amiko morgoshia <morgoshiaaamiko@gmail.com>
2026-07-08 20:30:49 +0400

    Initial commit

---
+++ Stage-2-Lesson-01-Homework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stage_2_Lesson_01_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
