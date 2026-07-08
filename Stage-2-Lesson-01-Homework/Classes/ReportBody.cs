16481d2f8da3981c5625d4bbd6bf4be110616b46
Morgosh <111243966+Mag1cccc@users.noreply.github.com>
2026-07-09 01:22:50 +0400

    feat: implement Facade (Structural) design pattern for HTML and PDF report generator (#2)

---
+++ Stage-2-Lesson-01-Homework/Classes/ReportBody.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage_2_Lesson_01_Homework.Classes
+{
+    internal class ReportBody
+    {
+        public string GenerateHTML()
+        {
+            return "<body> \n Video provides a powerful way to help you prove your point. When you click \n Online Video, you can paste in the embed code for the video you want to add. \n</body>";
+        }
+
+        public string GeneratePDF()
+        {
+            return "Body: \nVideo provides a powerful way to help you prove your point. When you click\r\nOnline Video, you can paste in the embed code for the video you want to add.\r\nYou can also type a keyword to search online for the video that best fits your\r\ndocument. To make your document look professionally produced, Word provides";
+        }
+    }
+}
---
+++ Stage-2-Lesson-01-Homework/Classes/ReportFacade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage_2_Lesson_01_Homework.Classes
+{
+    internal class ReportFacade
+    {
+        private ReportHeader header;
+        private ReportBody body;
+        private ReportFooter footer;
+
+        public ReportFacade()
+        { 
+            header = new ReportHeader();
+            body = new ReportBody();
+            footer = new ReportFooter();
+        }
+        public string GenerateHTMLReport() 
+        {
+            return header.GenerateHTML() + "\n" +
+               body.GenerateHTML() + "\n" +
+               footer.GenerateHTML();
+        }
+
+        public string GeneratePDFReport() {
+            return header.GeneratePDF() + "\n" +
+               body.GeneratePDF() + "\n" +
+               footer.GeneratePDF();
+        }
+    }
+}
