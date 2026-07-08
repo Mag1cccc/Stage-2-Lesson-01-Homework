b631f2491db14b82b30a094c039893e1cd828022
amiko morgoshia <morgoshiaaamiko@gmail.com>
2026-07-09 00:29:33 +0400

    feat: implement Proxy (Structural) design pattern for actor and stuntman scenario

---
+++ Stage-2-Lesson-01-Homework/interfaces/IPerfomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage_2_Lesson_01_Homework.interfaces
+{
+    internal interface IPerfomer
+    {
+        void Perform();
+    }
+}
